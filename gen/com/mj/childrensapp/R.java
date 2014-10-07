d20d9cf495d67737a87af159fcc63b677f7bea02
mjones2930 <markwjonesii@gmail.com>
2014-10-07 09:51:54 -0500

    added necessary pages

--- gen/com/mj/childrensapp/R.java
+++ gen/com/mj/childrensapp/R.java
@@ -28,15 +28,28 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_settings=0x7f030002;
+        public static final int activity_tuturial=0x7f030003;
+        public static final int activity_tuturial2=0x7f030004;
+        public static final int youwinpopup=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int game=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int settings=0x7f070002;
+        public static final int tuturial=0x7f070003;
+        public static final int tuturial2=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_game=0x7f050004;
+        public static final int title_activity_settings=0x7f050005;
+        public static final int title_activity_tuturial=0x7f050003;
+        public static final int title_activity_tuturial2=0x7f050006;
     }
     public static final class style {
         /** 
